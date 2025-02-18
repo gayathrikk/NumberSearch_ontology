9d431290ce50c059bd5d164538f3e2eb79d15aab
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2025-02-18 15:59:49 +0530

    NumberSearchOntology.java

--- src/test/java/com/test/Tagit/NumberSearchOntology.java
+++ src/test/java/com/test/Tagit/NumberSearchOntology.java
@@ -33,10 +33,10 @@ public class NumberSearchOntology {
 		driver.manage().window().maximize();
 		System.out.println("The server is Opened sucessfully");
 		WebDriverWait wait = new WebDriverWait(driver, 50);
-		WebElement viewerSectionLink = wait
-				.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@class, 'ng-tns-c93-3')]")));
-		viewerSectionLink.click();
-		System.out.println("The Viewer Icon is clicked");
+		// WebElement viewerSectionLink = wait
+		// 		.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@class, 'ng-tns-c93-3')]")));
+		// viewerSectionLink.click();
+		// System.out.println("The Viewer Icon is clicked");
 		String parentWindow = driver.getWindowHandle();
 		WebDriverWait wait1 = new WebDriverWait(driver, 20);
 		WebElement login = wait1
